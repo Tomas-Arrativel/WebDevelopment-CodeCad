add0a3b0d45b7562489c418a819e001204559282
Tomás Arrativel <arrativeltomas@gmail.com>
2024-02-27 08:09:57 -0300

    Tasks 4 and 5

--- Javascript challenges/rock-paper-scissors.js
+++ Javascript challenges/rock-paper-scissors.js
@@ -2,11 +2,21 @@ console.log('hi');
 
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-  if (
-    userInput === 'rock' ||
-    userInput === 'paper' ||
-    userInput === 'scissors'
-  ) {
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors')
     return userInput;
-  } else console.log("You haven't give a valid value");
+  else console.log("You haven't give a valid value");
 };
+
+const getComputerChoice = () => {
+  const choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
+
+getUserChoice('Paper');
