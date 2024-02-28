d1b2252703a733e7ecc98725ccd8f5dd0eb17d46
Tomás Arrativel <arrativeltomas@gmail.com>
2024-02-28 09:15:40 -0300

    tasks 6 to 9

--- Javascript challenges/whale-talk.js
+++ Javascript challenges/whale-talk.js
@@ -4,5 +4,9 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-  console.log(input[i]);
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) resultArray.push(vowels[j]);
+  }
 }
+
+console.log(resultArray);
