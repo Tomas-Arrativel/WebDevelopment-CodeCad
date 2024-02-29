2150edb3b4b377a9ec7e3958d8a7db53e67e8da6
Tomás Arrativel <arrativeltomas@gmail.com>
2024-02-29 18:47:16 -0300

    Task 1 to 3 completed

--- Javascript challenges/credit-card-checker.js
+++ Javascript challenges/credit-card-checker.js
@@ -20,10 +20,36 @@ const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
-
+const batch = [
+  valid1,
+  valid2,
+  valid3,
+  valid4,
+  valid5,
+  invalid1,
+  invalid2,
+  invalid3,
+  invalid4,
+  invalid5,
+  mystery1,
+  mystery2,
+  mystery3,
+  mystery4,
+  mystery5,
+];
 
 // Add your functions below:
+const validateCred = (arr) => {
+  arr.reverse();
+  let numbersToSum = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 !== 0) {
+      const multipliedNum = arr[i] * 2;
+      if (multipliedNum >= 10) numbersToSum.push(multipliedNum - 9);
+      else numbersToSum.push(multipliedNum);
+    } else numbersToSum.push(arr[i]);
+  }
+  console.log(numbersToSum);
+};
 
-
-
+validateCred(valid1);
