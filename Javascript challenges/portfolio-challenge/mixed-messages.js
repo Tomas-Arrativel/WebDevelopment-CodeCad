67a2ce9f994719b3dd30d426bb7776faa976e1e6
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-03 14:19:35 -0300

    Adding more phrases to the returned message

--- Javascript challenges/portfolio-challenge/mixed-messages.js
+++ Javascript challenges/portfolio-challenge/mixed-messages.js
@@ -1,15 +1,15 @@
-const motivation = [
-  'never give up',
-  'embrace the discomfort',
-  'strive for progress, not perfection',
-  'believe in your potential',
-  'challenge yourself every day',
-  'prioritize your health and fitness',
-  'set goals and crush them',
-  'push past your limits',
-  'stay consistent and dedicated',
-  'be proud of every step forward',
-];
+const typeOfPhrase = {
+  motivation: [
+    'never give up',
+    'embrace the discomfort',
+    'strive for progress, not perfection',
+    'believe in your potential',
+    'challenge yourself every day',
+    'prioritize your health and fitness',
+  ],
+  signInfo: ['star', 'moon', 'sun', 'comet'],
+  advice: ['go out and eat', 'not read this', 'play more', 'trust no one'],
+};
 
 const randomNum = (num) => {
   return Math.floor(Math.random() * num);
