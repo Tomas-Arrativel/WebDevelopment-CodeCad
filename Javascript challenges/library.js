e78fb4fb8722db544d3d8baf3b101ac1e5ba2dda
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-15 11:08:05 -0300

    Creating class media

---
+++ Javascript challenges/library.js
@@ -0,0 +1,30 @@
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    get title() {
+      return this._title;
+    }
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+    get ratings() {
+      return this._ratings;
+    }
+  
+    set isCheckedOut(newValue) {
+      this._isCheckedOut = newValue;
+    }
+  
+    toggleCheckOutStatus() {
+      this._isCheckedOut = !this._isCheckedOut;
+    }
+    getAverageRating() {
+      this._ratings.reduce((a, b) => a + b, 0)
+    }
+    addRating(rating) {
+      this._ratings.push(rating);
+    }
+  }
