c81fee1013cd8e1ba4a563ba35e29b0910abfa5c
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-19 17:35:40 -0300

    task 9

--- Javascript challenges/animal-fun-facts.jsx
+++ Javascript challenges/animal-fun-facts.jsx
@@ -8,6 +8,21 @@ const background = (
   <img className='background' src='./images/ocean.jpg' alt='ocean' />
 );
 
+let images = [];
+for (const animal in animals) {
+  const image = (
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role='button'
+    />
+  );
+  images.push(image);
+}
+
 const animalFacts = (
   <div>
     <h1>{title === '' ? 'Click an animal for a fun fac' : title}</h1>
