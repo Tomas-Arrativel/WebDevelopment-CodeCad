64cb073127b7a54807083ce69c508ca1e98a76fa
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-19 17:45:02 -0300

    final touches and tasks 13 and 14

--- Javascript challenges/animal-fun-facts.jsx
+++ Javascript challenges/animal-fun-facts.jsx
@@ -1,5 +1,8 @@
-const container = document.getElementById('app');
+import { animals } from './animals';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
+const container = document.getElementById('app');
 const root = createRoot(container);
 
 const title = '';
@@ -7,6 +10,14 @@ const title = '';
 const background = (
   <img className='background' src='./images/ocean.jpg' alt='ocean' />
 );
+
+const displayFact = (e) => {
+  const animalName = e.target.alt;
+  const animal = animals[animalName];
+  const optionIndex = Math.floor(Math.random() * animal.facts.length);
+  const funFact = animal.facts[optionIndex];
+  document.getElementById('fact').innerHTML = funFact;
+};
 
 let images = [];
 for (const animal in animals) {
@@ -18,22 +29,18 @@ for (const animal in animals) {
       src={animals[animal].image}
       aria-label={animal}
       role='button'
+      onClick={displayFact}
     />
   );
   images.push(image);
 }
 
-const displayFact = (e) => {
-  const animal = e.target.alt;
-  const optionIndex = Math.floor(Math.random() * animal.facts.length);
-  const funFact = animal.facts[optionIndex];
-};
-
 const animalFacts = (
   <div>
-    <h1>{title === '' ? 'Click an animal for a fun fac' : title}</h1>
+    <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
     {background}
     <div className='animals'>{images}</div>
+    <p id='fact'></p>
   </div>
 );
 
