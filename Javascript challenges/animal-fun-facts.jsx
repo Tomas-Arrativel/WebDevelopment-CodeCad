59de94f364f4c269fff6afa2ae75d462e7f1e00f
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-19 17:25:34 -0300

    tasks 7 and 8

--- Javascript challenges/animal-fun-facts.jsx
+++ Javascript challenges/animal-fun-facts.jsx
@@ -3,8 +3,16 @@ const container = document.getElementById('app');
 const root = createRoot(container);
 
 const title = '';
+
+const background = (
+  <img className='background' src='./images/ocean.jpg' alt='ocean' />
+);
+
 const animalFacts = (
-  <h1>{title === '' ? 'Click an animal for a fun fac' : title}</h1>
+  <div>
+    <h1>{title === '' ? 'Click an animal for a fun fac' : title}</h1>
+    {background}
+  </div>
 );
 
 root.render(animalFacts);
