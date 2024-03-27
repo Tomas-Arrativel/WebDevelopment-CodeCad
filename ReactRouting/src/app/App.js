4b0c3af2fe56b1e95cdfce278271c9cbbef817bf
Tomás Arrativel <arrativeltomas@gmail.com>
2024-03-27 11:24:49 -0300

    task 2

--- ReactRouting/src/app/App.js
+++ ReactRouting/src/app/App.js
@@ -1,32 +1,29 @@
-import React from "react";
-import About from "../components/About";
-import SignUp from "../components/SignUp";
-import Articles from "../components/Articles";
-import Article from "../components/Article";
-import Categories from "../components/Categories";
-import Category from "../components/Category";
-import Author from "../components/Author";
-import Profile from "../components/Profile";
-import EditProfileForm from "../components/EditProfileForm";
-import Root from "../components/Root";
+import React from 'react';
+import About from '../components/About';
+import SignUp from '../components/SignUp';
+import Articles from '../components/Articles';
+import Article from '../components/Article';
+import Categories from '../components/Categories';
+import Category from '../components/Category';
+import Author from '../components/Author';
+import Profile from '../components/Profile';
+import EditProfileForm from '../components/EditProfileForm';
+import Root from '../components/Root';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { Route } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 
-const router = 
+const router = createBrowserRouter(
   /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
+  <Route path='/' element={<Root />}>
     {/* Add Routes here! */}
-  </Route>
+  </Route>,
+);
 
 function App() {
-  return (
-    <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
-    </>
-  );
+  return <>{/* Replace below and add Router Provider*/}</>;
 }
 
 export default App;
