88a5d88bdcb8e19581327a76d648eb9b0fe1d0b8
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-02 18:00:49 -0300

    Using Navigate to redirect user when not logged in

--- ReactRouting/src/components/Profile.js
+++ ReactRouting/src/components/Profile.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Outlet, Navigate } from 'react-router-dom';
 import {
   selectCurrentUser,
   selectIsLoggedIn,
 } from '../features/session/sessionSlice';
-import { Outlet } from 'react-router-dom';
 
 export default function Profile() {
   const currentUser = useSelector(selectCurrentUser);
   const loggedIn = useSelector(selectIsLoggedIn);
-
   // use loggedIn to return a Navigate
+  if (!loggedIn) {
+    return <Navigate to='/sign-up' />;
+  }
 
   return (
     <main>
