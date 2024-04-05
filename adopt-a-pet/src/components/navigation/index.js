fdba8c87c5131c5978911d59d6cfb10e7e950ddf
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-05 18:07:56 -0300

    Using NavLink to navigate to through the links with the server

--- adopt-a-pet/src/components/navigation/index.js
+++ adopt-a-pet/src/components/navigation/index.js
@@ -3,7 +3,7 @@ import { getPetTypes } from '../../api/petfinder';
 import Logo from '../../assets/logo.svg';
 import Search from '../search';
 
-// Import NavLink
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   const [petTypes, setPetTypes] = useState([]);
@@ -19,28 +19,28 @@ const Navigation = () => {
 
   return (
     <nav>
-      <div className="nav-logo">
-        <img src={Logo} alt="Petlover" />
+      <div className='nav-logo'>
+        <img src={Logo} alt='Petlover' />
         <Search />
       </div>
-      <ul className="nav-links">
+      <ul className='nav-links'>
         <li key={'all'}>
           {/* These links should be NavLink component and add a special active class name if its an active link */}
-          <a href="/"
-            className='nav-link'
-          >
+          <NavLink to='/' className='nav-link'>
             All Pets
-          </a>
+          </NavLink>
         </li>
         {petTypes
           ? petTypes.map((type) => (
               <li key={type.name}>
                 {/* These links should be NavLink component and add a special active class name if its an active link */}
-                <a href={`/${type._links.self.href.split('/').pop()}`}
+                <NavLink
+                  to={`/${type._links.self.href.split('/').pop()}`}
                   key={type.name}
-                  className='nav-link'               >
+                  className='nav-link'
+                >
                   {type.name}s
-                </a>{' '}
+                </NavLink>{' '}
               </li>
             ))
           : 'Loading...'}
