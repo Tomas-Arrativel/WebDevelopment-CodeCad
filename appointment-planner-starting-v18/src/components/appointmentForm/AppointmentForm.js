29f77efe5f50d76a1920160c88d95b4386fb0ef0
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-23 19:33:50 -0300

    Task 8

--- appointment-planner-starting-v18/src/components/appointmentForm/AppointmentForm.js
+++ appointment-planner-starting-v18/src/components/appointmentForm/AppointmentForm.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
+import { ContactPicker } from '../contactPicker/ContactPicker';
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
-  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  const [month, day, year] = new Date().toLocaleDateString('en-US').split('/');
+  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
 };
 
 export const AppointmentForm = ({
@@ -17,10 +16,29 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
+  const nameOnChange = (e) => setTitle(e.target.value);
+  const dateOnChange = (e) => setDate(e.target.value);
+  const timeOnChange = (e) => setTime(e.target.value);
+  const contactOnChange = (e) => setContact(e.target.value);
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={title} onChange={nameOnChange} />
+      <input
+        type='date'
+        min={getTodayString}
+        value={date}
+        onChange={dateOnChange}
+      />
+      <input type='time' value={time} onChange={timeOnChange} />
+      <ContactPicker
+        contacts={contacts}
+        onChange={contactOnChange}
+        value={contact}
+      />
+      <input type='submit' value='Send' />
+    </form>
   );
 };
